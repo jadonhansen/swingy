62b1a8cc7ad667b5356f4ca9d6246586dbd0f3c9
jadonhansen <jadongavhansen@gmail.com>
2020-09-08 11:04:46 +0200

    new menu gui

--- src/com/swingy/view/Menu.java
+++ src/com/swingy/view/Menu.java
@@ -20,7 +20,7 @@ public class Menu implements ActionListener {
     public Menu(ArrayList<Hero> heroes, Controller controller) {
         this.controller = controller;
         this.heroes = heroes;
-        roof = 50 + heroes.size() * 110;
+        roof = 110 + heroes.size() * 190;
     }
 
     public void displayMenu() {
@@ -51,8 +51,8 @@ public class Menu implements ActionListener {
     }
 
     public void heading() {
-        JLabel heading = new JLabel("SWINGY");
-        heading.setBounds(10, 10, 55, 20);
+        JLabel heading = new JLabel(" SWINGY");
+        heading.setBounds(10, 10, 56, 20);
         heading.setOpaque(true);
         heading.setBackground(Color.CYAN);
         panel.add(heading);
@@ -91,34 +91,43 @@ public class Menu implements ActionListener {
     public void savedCharacters() {
         if (heroes.size() > 0) {
             JLabel exist = new JLabel("Load an existing character:");
-            exist.setBounds(10, 110, 200, 20);
+            exist.setBounds(10, 120, 200, 20);
             panel.add(exist);
         }
-//        try {
-//            for (int i = 0; i < heroes.size(); i++) {
-//                JLabel label = new JLabel("<html>Name: " + heroes.get(i).getName() + "<br/>" +
-//                        "Type: " + heroes.get(i).getType() + "<br/>" +
-//                        "Level: " + heroes.get(i).getLevel() + "<br/>" +
-//                        "Experience: " + heroes.get(i).getExperience() + "<br/>" +
-//                        "Attack Increase: " + heroes.get(i).getAttack() + "<br/>" +
-//                        "Defence Increase: " + heroes.get(i).getDefence() + "<br/>" +
-//                        "Hit Points Increase: " + heroes.get(i).getHitPoints() + "<br/>" +
-//                        "</html>");
-//
+
+        try {
+            for (int i = 0; i < heroes.size(); i++) {
+                JLabel label = new JLabel("<html>Name: " + heroes.get(i).getName() + "<br/>" +
+                        "Type: " + heroes.get(i).getType() + "<br/>" +
+                        "Level: " + heroes.get(i).getLevel() + "<br/>" +
+                        "Experience: " + heroes.get(i).getExperience() + "<br/>" +
+                        "Attack Increase: " + heroes.get(i).getAttack() + "<br/>" +
+                        "Defence Increase: " + heroes.get(i).getDefence() + "<br/>" +
+                        "Hit Points Increase: " + heroes.get(i).getHitPoints() + "<br/>" +
+                        "</html>");
+
 //                int k = i + 1;
 //                int y = (150 * k) - 80;
-//
-//                label.setBounds(10, y, 200, 110);
-//                panel.add(label);
-//
-//                JButton button = new JButton(heroes.get(i).getName() + " ID: " + heroes.get(i).getID());
-//                button.addActionListener(this);
-//                button.setBounds(10, y + 110, 150, 30);
-//                panel.add(button);
-//            }
-//        } catch (IndexOutOfBoundsException e) {
-//            System.out.println("Error while making labels -> Menu.java -> displayMenu(): " + e);
-//        }
+
+                if (i == 0) {
+                    label.setBounds(10, 150, 200, 130);
+                } else {
+                    label.setBounds(10, 150 + 30 + (i * 130), 200, 130);
+                }
+                panel.add(label);
+
+                JButton button = new JButton(heroes.get(i).getName() + " ID: " + heroes.get(i).getID());
+                button.addActionListener(this);
+                if (i == 0) {
+                    button.setBounds(10, 150 + 130, 150, 30);
+                } else {
+                    button.setBounds(10, 150 + 130 + 30 + (i * 130), 150, 30);
+                }
+                panel.add(button);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Error while making labels -> Menu.java -> displayMenu(): " + e);
+        }
     }
 
     @Override
